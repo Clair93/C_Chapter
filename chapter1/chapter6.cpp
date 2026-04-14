0ae24f51ebb9ef0edf1661aaf6274c95fc8d0268
Jaewon.Moon <mjw9313@gmail.com>
2026-04-14 17:00:08 +0900

    Fix 자가진단 add

--- chapter1/chapter6.cpp
+++ chapter1/chapter6.cpp
@@ -1,4 +1,137 @@
 
+// chapter6 자가진단01
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 1; i <= 15; i++) {
+		printf("%d ", i);
+	}
+	return 0;
+}
+
+
+// chapter6 자가진단02
+#include <stdio.h>
+int main()
+{
+	int a;
+	int sum = 0, b = 1;
+
+	scanf("%d", &a);
+
+	while (b<=a) {
+		sum += b;
+		b++;
+	}
+	printf("%d", sum);
+
+	return 0;
+}
+
+
+// chapter6 자가진단03
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	while (1) {
+		printf("number? ");
+		scanf("%d", &a);
+		if (a == 0) break;
+		if (a > 0) {
+			printf("positive integer\n");
+		}
+		else {
+			printf("negative number\n");
+		}
+	}
+	return 0;
+}
+
+
+// chapter6 자가진단04
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int sum = 0, cnt = 0;
+	double avg = 0;
+
+	while (1) {
+		scanf("%d", &a);
+		sum += a;
+		cnt++;
+		if (a == 100)break;
+	}
+	avg = (double)sum / cnt;
+
+	printf("%d\n", sum);
+	printf("%.1f", avg);
+
+	return 0;
+}
+
+
+// chapter6 자가진단05
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	do {
+		scanf("%d", &a);
+		if (a % 3 != 0)continue;
+		if (a % 3 == 0) {
+			printf("%d\n", a / 3);
+		}
+	} while (a != -1);
+	return 0;
+}
+
+
+// chapter6 자가진단06
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	do {
+		printf("1. Korea\n");
+		printf("2. USA\n");
+		printf("3. Japan\n");
+		printf("4. China\n");
+		printf("number? ");
+		scanf("%d", &a);
+
+		switch (a)
+		{
+		case 1 :
+			printf("\nSeoul\n\n");
+			break;
+		case 2:
+			printf("\nWashington\n\n");
+			break;
+		case 3:
+			printf("\nTokyo\n\n");
+			break;
+		case 4:
+			printf("\nBeijing\n\n");
+			break;
+		default:
+			printf("\nnone\n\n");
+			break;
+		}
+		
+	} while (a > 0 && a < 5);
+}
+
+
 // chapter6 형성평가01
 
 #include <stdio.h>
