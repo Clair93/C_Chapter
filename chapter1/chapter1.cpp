74f73419e9cf4656c639afe9b17b5e897399693c
Jaewon.Moon <mjw9313@gmail.com>
2026-04-20 18:04:37 +0900

    Fix chapter1 add selfcheck

--- chapter1/chapter1.cpp
+++ chapter1/chapter1.cpp
@@ -1,4 +1,103 @@
 ﻿
+// chapter1 자가진단 01
+#include <stdio.h>
+
+int main()
+{
+	printf("Fun Programming!");
+
+	return 0;
+}
+
+
+// chapter1 자가진단 02
+#include <stdio.h>
+
+int main()
+{
+	printf("Programming! It's fun.");
+
+	return 0;
+}
+
+
+// chapter1 자가진단 03
+#include <stdio.h>
+
+int main()
+{
+	printf("My name is Hong Gil Dong.\n");
+	printf("I am 13 years odl.");
+
+	return 0;
+}
+
+
+// chatper1 자가진단 04
+#include <stdio.h>
+
+int main()
+{
+	printf("(@) (@)\n");
+	printf("(=^.^=)\n");
+	printf("(-m-m-)\n");
+
+	return 0;
+}
+
+
+// chapter1 자가진단 05
+#include <stdio.h>
+
+int main()
+{
+	printf("I can program well.\n");
+	printf("Dreams come true.\n");
+
+	return 0;
+}
+
+
+// chapter1 자가진단 06
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n", "My height");
+	printf("%d\n", 170);
+	printf("%s\n", "My weight");
+	printf("%f\n", 68.6);
+
+	return 0;
+}
+
+
+// chapter1 자가진단 07
+#include <stdio.h>
+
+int main()
+{
+	printf("%d Dan\n", 5);
+	printf("%d * %d = %d", 5, 2, 5 * 2);
+
+	return 0;
+}
+
+
+// chapter1 자가진단 08
+#include <stdio.h>
+
+int main()
+{
+	printf("%10s%10s%10s\n", "item", "count", "price");
+	printf("%10s%10d%10d\n", "pen", 20, 100);
+	printf("%10s%10d%10d\n", "note", 5, 95);
+	printf("%10s%10d%10d\n", "eraser", 110, 97);
+
+	return 0;
+}
+
+
 // 형성평가 01번 문제 풀기 (정올)
 #include <stdio.h>
 
